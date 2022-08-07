1dd6a4fffadf6f14746a960277f044273979fed4
GianniMejia <101169251+GianniMejia@users.noreply.github.com>
2022-08-06 23:01:40 -0700

    added more values to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,17 +4,27 @@ INSERT INTO department(`name`)
 VALUES
 ('Billing'), 
 ('Human Resources'), 
-('Sales');
+('Sales'),
+(`Reception`),
+(`Records`),
+(`Mail Room`);
+
 
 INSERT INTO `role`(title, salary, department_id)
 VALUES
 ('Billing Agent', 40000, 1), 
 ('Human Resources Agent', 50000, 2), 
-('Sales Agent', 60000, 3);
+('Sales Agent', 60000, 3),
+('Receptionist', 30000, 4),
+('Bookkeeper', 35000, 5)
+('Mail Clerk', 25000, 6)
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
 ('Kevin', 'Bacon', 1, NULL), 
 ('Will', 'Smith', 2, 1), 
-('Chris', 'Rock', 3, 2);
+('Chris', 'Rock', 3, 2),
+('Adam', 'Sandler', 4, 2), 
+('Tom', 'Hanks', 6, 4),
+('Johnny Depp', 5, 3);
 
